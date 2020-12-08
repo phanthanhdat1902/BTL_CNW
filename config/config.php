9341cf226c75f5016ac3c45f8daa1572a77f91d5
phanthanhdat1902 <kt22hust@gmail.com>
2020-12-08 14:42:53 +0700

    update db

--- config/config.php
+++ config/config.php
@@ -5,9 +5,9 @@
 define ('DEVELOPMENT_ENVIRONMENT',true);
 
 
-define('DB_NAME', 'framework');
+define('DB_NAME', 'booking_db');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
 define('BASE_PATH','http://localhost/BTL_CNW'); // cái này t chưa tìm hiểu, cứ để tạm đây
