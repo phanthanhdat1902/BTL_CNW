cddaf8214aa8fd6ce099d2672cab22992460707c
TV Dinh <dinh.tv173017@sis.hust.edu.vn>
2020-12-14 04:11:01 +0700

    FrontEnd tour admin

---
+++ application/views/users/tourDetail.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Đặt tuor du lịch</title>
+    <link rel="stylesheet" href="/css/styleTuor.css" />
+    <link rel="stylesheet" href="/css/styleTourDetail.css" />
+    <script src="/js/scriptHome.js"></script>
+
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="logo">
+                <h4>LOGO</h4>
+            </div>
+            <ul class="navHeader" id="myMenu">
+                <li><a href="#">Khách sạn</a></li>
+                <li><a href="#">Tour</a></li>
+                <li><a href="#">Vé máy bay</a></li>
+                <li><a href="#">Cẩm nang du lịch</a></li>
+                <li><a href="#">...</a>
+                    <ul class="subMenu">
+                        <li><a href="#">Tài khoản đặt tour</a></li>
+                        <li><a href="#">Ưu đãi đặc biệt</a></li>
+                        <li><a href="#">Giới thiệu</a></li>
+                        <li><a href="#">Hỏi đáp</a></li>
+                        <li><a href="#">Hỗ trợ</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="userControl">
+                <a href="#">Đăng ký</a>
+                <a href="#">Đăng nhập</a>
+                <span id="btnMenu" onclick="showMenu()">&#9776;</span>
+            </div>
+        </div>
+    </div>
+    <div class="tourHomeHeader">
+        <div class="container">
+            <div class="">
+                <div class="title">
+                    <h1>Du lịch theo phong cách riêng</h1>
+                    <h2>Trải nghiệm trọn vẹn - Giá cả phải chăng.</h2>
+                </div>
+            </div>
+            <div class="searchForm">
+                <input type="text" placeholder="search here ..." style="width: 100%;">
+                <input type="date">
+                <input type="text" placeholder="Khởi hành từ">
+                <input type="button" class="btnSearch" value="Tìm kiếm">
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="contentHeader">
+            <ol>
+                <li>Trang chủ</li>
+                <li>Bến Tre</li>
+                <li>Tour Miền Tây 2N1D: Bến Tre - Trà Vinh - Cồn Chim Tết</li>
+            </ol>
+        </div>
+        <div class="pageTitle">
+            <span>Tour Miền Tây 2N1D: Bến Tre - Trà Vinh - Cồn Chim Tết</span>
+        </div>
+        <div class="container">
+            <div class="bookingForm">
+                <div>
+                    <h1>Đặt ngay, chỉ 2 phút. Hoặc gọi (028) 3933 8002</h1>
+                </div>
+                <div>
+                    <span>Chọn ngày khởi hành: </span>
+                    <input type="date">
+                </div>
+                <div>
+                    <span>2 Người lớn</span>
+                    <span>x 6.290.000 - +</span>
+                </div>
+                <div>
+                    <span>0 Trẻ em</span>
+                    <span>- +</span>
+                </div>
+                <div>
+                    <span>Tổng cộng</span>
+                    <span>12.580.000 VND</span>
+                </div>
+
+                <button type="submit">Yêu cầu đặt</button>
+            </div>
+            <div class="tourSideBar">
+                <ul>
+                    <li>Chương trình tour</li>
+                    <li>Lịch khởi hành</li>
+                    <li>Điều khoản & quy định</li>
+                </ul>
+            </div>
+
+            <div class="tourHeaderContainer">
+                <div class="tourHeaderImage"></div>
+                <div class="tourHeaderInfor">
+                    <span>Hồ Chí Minh</span>
+                    <span>5 Ngày 4 Đêm</span>
+                    <span>Phương tiện: Bus - Máy bay</span>
+                    <span>Mã Tour: TO1256</span>
+                </div>
+            </div>
+            <div class="tourDetailMain">
+                <h1>Thung Lũng Yên Bình - Thơ Mộng</h1>
+                <span>Du lịch Hòa Bình hấp dẫn du khách bởi những nét văn hóa dân tộc vô cùng đặc sắc. Sự đa dạng của các dân tộc nơi đây đã tạo nên những nét quyến rũ độc đáo. Bên cạnh đó du lịch Hòa Bình còn đưa đến cho bạn những góc nhìn khác lạ về Phong Cảnh hùng vĩ tráng lệ. Hang Kia Pà Cò là thung lũng xinh đẹp của huyện Mai Châu - Hòa Bình làm thổn thức bao con tim của người lữ khách. Nơi đây không chỉ quyến rũ bởi sự mộc mạc của núi rừng, sự e ấp của những cô sơn nữ mà còn hấp dẫn bởi những tấm lòng thân thương đầy tình nghĩa của đồng bào dân tộc. </span>
+                <h1>Những trải nghiệm thú vị trong chương trình</h1>
+                <span>Hành trình mới khám phá những điểm đến còn mang nguyên vẹn nét đẹp hoang sơ:</span>
+                <ul>
+                    <li>Khám phá nét hoang sơ, quyến rũ của thung lũng Hang Kia - Pà Cò</li>
+                    <li>Trải nghiệm cuộc sống của người dân tộc Mông với các hoạt động: giã bánh dày, nhuộm vải, vẽ sáp ong.</li>
+                    <li>Thỏa sức check in trên những đồi chè bát ngát hay những vườn hoa đào, hoa mận tuyệt đẹp tựa như một Mộc Châu thu nhỏ.</li>
+                </ul>
+            </div>
+            <div class="departureSchedule">
+                <h1>Lịch khởi hành</h1>
+                <table>
+                    <thead>
+                        <th>Ngày khởi hành</th>
+                        <th>Ngày về</th>
+                        <th>Tình trạng</th>
+                        <th>Giá</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>T4, 09/12/2020</td>
+                            <td>CN, 13/12/2020</td>
+                            <td>Liên hệ</td>
+                            <td>6.290.000 VND</td>
+                        </tr>
+                        <tr>
+                            <td>T4, 09/12/2020</td>
+                            <td>CN, 13/12/2020</td>
+                            <td>Liên hệ</td>
+                            <td>6.290.000 VND</td>
+                        </tr>
+                        <tr>
+                            <td>T4, 09/12/2020</td>
+                            <td>CN, 13/12/2020</td>
+                            <td>Liên hệ</td>
+                            <td>6.290.000 VND</td>
+                        </tr>
+                        <tr>
+                            <td>T4, 09/12/2020</td>
+                            <td>CN, 13/12/2020</td>
+                            <td>Liên hệ</td>
+                            <td>6.290.000 VND</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="tourService">
+                <h1>Dịch vụ đi kèm</h1>
+                <ul>
+                    <li>Bảo hiểm</li>
+                    <li>Bữa ăn</li>
+                    <li>Hướng dẫn viên</li>
+                    <li>Vé tham quan</li>
+                    <li>Vé dưa đón</li>
+                </ul>
+            </div>
+
+            <div class="subPolicy">
+                <h1>Chính sách phụ thu</h1>
+                <ul>
+                    <li>Phụ thu phòng riêng 450.000 vnđ/khách.</li>
+                    <li>Khách nước ngoài: 350.000 vnđ/khách.</li>
+                    <li>Tết dương lịch: không phụ thu.</li>
+                    <li>Tết âm lịch: 250.000 vnđ/khách</li>
+                    <li>Trẻ em từ 10 tuổi trở lên tính 100% giá tour.</li>
+                    <li>Trẻ em từ 5 – 9 tuổi tính 75% giá tour.</li>
+                    <li>Trẻ em dưới 4 tuổi miễn phí giá tour.</li>
+                </ul>
+            </div>
+            
+            <div class="tourCancellationRules">
+                <h1>Điều khoản</h1>
+                <div class="tab">
+                    <button class="tablinks" onclick="openTabRules(event, 'tab1')" id="defaultOpen">Bao gồm</button>
+                    <button class="tablinks" onclick="openTabRules(event, 'tab2')">Không bao gồm</button>
+                    <button class="tablinks" onclick="openTabRules(event, 'tab3')">Hủy đổi</button>
+                    <button class="tablinks" onclick="openTabRules(event, 'tab4')">Lưu ý</button>
+                </div>
+
+                <div id="tab1" class="tabcontent">
+                    <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+                    <p>
+                        Vận chuyển:
+                        - Xe tham quan (16, 29, 35, 45 chỗ tùy theo số lượng) đón - tiễn và phục vụ theo chương trình.
+                        - Vé máy bay khứ hồi: Viet jet Air ( 07 kg sách tay + 20 kg hành lý ký gửi/kiện).
+                        Lưu trú:
+                        - Khách sạn 3 sao: 2 – 3 khách/phòng (trường hợp lẻ nam, nữ ở phòng 03).</p>
+                </div>
+
+                <div id="tab2" class="tabcontent">
+                    <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+                    <p>- Tiền Tip cho HDV/ lái xe,
+                        - Ăn uống, vui chơi giải trí cá nhân .. ngoài chương trình.
+                        - 01 bữa ăn tối tại Sapa (bữa tối ,ngày thứ 2 theo chương trình).</p>
+                </div>
+
+                <div id="tab3" class="tabcontent">
+                    <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+                    <p>Chính sách hủy:
+                        Mọi sự thay đổi liên quan đến vé đã xuất: ngày giờ đi, tên hành khách, hủy vé, vui lòng chịu chi
+                        phí theo qui định sau:
+                        - Ngay sau khi đăng ký tour nếu hủy sẽ bị phạt tiền tour và vé máy bay: </p>
+                </div>
+                <div id="tab4" class="tabcontent">
+                    <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+                    <p>- Trong những trường hợp bất khả kháng như: khủng bố, bạo động, thiên tai, lũ lụt. dịch bệnh… Tuỳ
+                        theo tình hình thực tế và sự thuận tiện, an toàn của khách hàng, công ty sẽ chủ động thông báo
+                        cho khách hàng sự thay đổi như sau:
+                        huỷ hoặc thay thế bằng một chương trình mới với chi phí tương đương chương trình tham quan trước
+                        đó.
+                        Trong trường hợp chương trình mới có phát sinh thì Khách hàng sẽ thanh toán khoản phát sinh này.
+                        Tuy nhiên, mỗi bên có trách nhiệm cố gắng tối đa, giúp đỡ bên bị thiệt hại nhằm giảm thiểu các
+                        tổn thất gây ra vì lý do bất khả kháng.…</p>
+                </div>
+            </div>
+
+            <div>
+                <h1>Đánh giá gần đây</h1>
+                <div>
+                    <h2>Trang Nguyễn</h2>
+                    <span>
+                        8.0 Rất tốt 24-06-2020Tour tư vấn ban đầu có 25 khách nhưng khi đến sân bay là 45 khách. Tour đi
+                        quá
+                        đông, hdv không thể chăm sóc tốt hết được
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
+            <a href="#">Trần Văn Định</a>.
+        </p>
+    </div>
+</body>
+
+</html>
